92064412b0ddb431edf039d641a0740d49f281bf
Camila <camilasoich7@gmail.com>
2026-08-13 12:44:56 -0300

    Clases de partidos, ciudad y estadio

---
+++ src/model/Stadium.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Stadium {
+    private boolean used;
+    private String name;
+    private int capacity;
+    private City city;
+
+
+    public Stadium (String name, int capacity, City city) {
+        this.used = false;
+        this.name = name;
+        this.capacity = capacity;
+        this.city = city;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setUsed(boolean used){
+        this.used = used;
+    }
+}
--- src/model/Zone.java
+++ src/model/Zone.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 
 public class Zone {
     private ArrayList<Team> teams;
+    private ArrayList<ZoneMatch> matches;
 
     public Zone() {
         this.teams = new ArrayList<>();
+        this.matches = new ArrayList<>(6);
     }
 
     public ArrayList<Team> getTeams() {
         return teams;
     }
 
+    public ArrayList<ZoneMatch> getMatches() {
+        return matches;
+    }
+
     public void addTeam(Team team) {
         if (teams.size() < 4) {
             teams.add(team);
         }
+    }
+
+    public void addMatch (ZoneMatch match){
+        if (matches.size()<6)
+            matches.add(match);
     }
 }

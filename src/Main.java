1a1160b37d0e1e4e8b5a3eedf8dbf6cba4347e3f
Camila <camilasoich7@gmail.com>
2026-09-11 16:43:04 -0300

    Cargar BD y leer los estadios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import model.MatchSimulator;
+import model.StadiumLoader;
 import model.Team;
 import model.Tournament;
 import model.match.Formation;
@@ -23,6 +24,8 @@ import static model.MatchSimulator.simulateMatch;
 public class Main {
     public static void main(String[] args) throws Exception {
         Tournament tournament = fileReader("torneo.json");
+        StadiumLoader loader = new StadiumLoader();
+        tournament.addAllStadiums(loader.loadStadiums());
         tournament.zoneDraw();
         tournament.generateGroupStageMatches();
         //simulateGroupStage(tournament);
